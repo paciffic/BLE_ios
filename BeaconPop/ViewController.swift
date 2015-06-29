1211ff11c4c20d02de85992804c471174626498b
paciffic <paciffic@gmail>
2015-06-29 14:46:58 +0900

    first commit

--- BeaconPop/ViewController.swift
+++ BeaconPop/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import QuartzCore
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+
+    @IBOutlet weak var btnAction: UIButton!
+    
+    @IBOutlet weak var lblStatus: UILabel!
+    
+    @IBOutlet weak var lblBTStatus: UILabel!
+    
+    @IBOutlet weak var txtMajor: UITextField!
+    
+    @IBOutlet weak var txtMinor: UITextField!
+    
+    let uuid = NSUUID(UUIDString: "55C1F3C8-D9E0-4A82-8731-C8CAC9FE8739")
+    var beaconRegion: CLBeaconRegion!
+    var bluetoothPeripheralManager: CBPeripheralManager!
+    var isBroadcasting = false
+    var dataDictoionary = NSDictionary()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnAction.layer.cornerRadius = btnAction.frame.size.width / 2
+        
+        var swipeDownGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleSwipeGestureRecognizer:")
+        swipeDownGestureRecognizer.direction = UISwipeGestureRecognizerDirection.Down
+        view.addGestureRecognizer(swipeDownGestureRecognizer)
+        
+        bluetoothPeripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,84 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // MARK: Custom method implementation
+    
+    func handleSwipeGestureRecognizer(gestureRecognizer: UISwipeGestureRecognizer) {
+        txtMajor.resignFirstResponder()
+        txtMinor.resignFirstResponder()
+    }
+    
+    
+    // MARK: IBAction method implementation
+    
+    @IBAction func switchBroadcastingState(sender: AnyObject) {
+        println("Button was pushed!!")
+        
+        if txtMajor.text == "" || txtMinor.text == "" {
+            return
+        }
+        
+        if txtMajor.isFirstResponder() || txtMinor.isFirstResponder() {
+            return
+        }
+        
+        if !isBroadcasting {
+            if bluetoothPeripheralManager.state == CBPeripheralManagerState.PoweredOn {
+                let major: CLBeaconMajorValue = UInt16(txtMajor.text.toInt()!)
+                let minor: CLBeaconMinorValue = UInt16(txtMinor.text.toInt()!)
+                beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: "com.paciffic.beacondemo")
+                dataDictoionary = beaconRegion.peripheralDataWithMeasuredPower(nil)
+                bluetoothPeripheralManager.startAdvertising(dataDictoionary)
+            }
+            btnAction.setTitle("Stop", forState: UIControlState.Normal)
+            lblStatus.text = "Broadcasting..."
+            
+            txtMajor.enabled = false
+            txtMinor.enabled = false
+            isBroadcasting = true
+        }
+        else {
+            bluetoothPeripheralManager.stopAdvertising()
+            
+            btnAction.setTitle("Start", forState: UIControlState.Normal)
+            lblStatus.text = "Stopped"
+            
+            txtMajor.enabled = true
+            txtMinor.enabled = true
+            
+            isBroadcasting = false
+        }
+        
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        var statusMessage = ""
+        
+        switch peripheral.state {
+        case CBPeripheralManagerState.PoweredOn:
+            statusMessage = "Bluetooth Status: Turned On"
+        case CBPeripheralManagerState.PoweredOff:
+            if isBroadcasting {
+                switchBroadcastingState(self)
+            }
+            statusMessage = "Bluetooth Status: Turned Off"
+            
+        case CBPeripheralManagerState.Resetting:
+            statusMessage = "Bluetooth Status: Resetting"
+            
+        case CBPeripheralManagerState.Unauthorized:
+            statusMessage = "Bluetooth Status: Not Authorized"
+            
+        case CBPeripheralManagerState.Unsupported:
+            statusMessage = "Bluetooth Status: Not Supported"
+            
+        default:
+            statusMessage = "Bluetooth Status: UnKnown"
+        }
+        
+        lblBTStatus.text = statusMessage
+    }
+    
 }
 
